02fae9c867e5f78441ef2762f6003d429eb82589
Miguel Ramirez <miguelangelramirezcarvajal@gmail.com>
2024-08-11 11:31:04 -0600

    add comments to 242

--- 242.py
+++ 242.py
@@ -6,14 +6,17 @@ class Solution:
         #create a dictionary to store the frequency of each character in s and t
         charFreq = {}
         
+        # fill the dictionary with the frequency of each character in s
         for char in s:
             charFreq[char] = charFreq.get(char, 0) + 1
-        
+        #check if the frequency of each character in t is the same as the 
+        #frequency of the character in s
         for char in t:
             if char not in charFreq or charFreq[char] == 0:
                 return False  
             charFreq[char] -= 1  
         
-        
+        #if the frequency of each character in t is the same as the frequency of 
+        #the character in s return True
         return True
              
